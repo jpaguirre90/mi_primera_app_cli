f0996e3a42f40c558320c0e3bd82398c05386a64
JP Aguirre <jp.aguirre90@gmail.com>
2020-07-14 20:54:37 -0400

    mi_primera_app_cli

---
+++ src/components/MiLista.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h5 class="card-title">Peliculas que {{nombre}} desea ver (otra vez)</h5>
+        <ul>
+            <li v-for="(pelicula,i) in peliculas" v-bind:key="i">{{i+1}}. {{pelicula}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'MiLista',
+  props: {
+    msg: String
+  },
+  data:function(){
+    return{
+      nombre:'JP',
+      peliculas:['Pacific Rim','Jurassic Park','Back to the Future','Dawn of the dead','Police Academy']
+    }
+  }
+}
+</script>
+<style scoped>
+    li {
+        background-color: red;
+    }
+</style>
